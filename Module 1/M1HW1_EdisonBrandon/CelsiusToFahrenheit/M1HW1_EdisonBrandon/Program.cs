f52df38c324a041356f8a8f5d2a440c808b377ad
Brandon E <55399973+edisonb9516@users.noreply.github.com>
2021-08-23 19:31:30 -0400

    Added Module 1
    This is adding the first half of the module 1 homework

---
+++ Module 1/M1HW1_EdisonBrandon/CelsiusToFahrenheit/M1HW1_EdisonBrandon/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/**
+* 8/23/21
+* CSC 253
+* Brandon Edison
+* This program is going to convert the celsius temperatures to fahrenheit. This will list 0-20 degrees celsius. 
+* THis will also list then in a table type format. 
+*/
+namespace M1HW1_EdisonBrandon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //greet the user 
+            Console.WriteLine("Welcome to Celsius to Fahrenheit! \n" +
+                "This program will list in a table the conversion of Celsius to Fahrenheit from 0 degrees to 20.");
+
+            //Create a table header
+            Console.WriteLine();
+            Console.WriteLine("Celsius      Fahrenheit");
+            Console.WriteLine("------------------------------------");
+
+            //create a for loop to allow the table to be created and the conversion to start
+            for (int counter = 0; counter <= 20; counter++){
+
+                //display the celsius first
+                Console.WriteLine("     ");
+                Console.Write(counter.ToString("0° C"));
+                Console.Write("             ");
+
+                //convert the counter degree to fahrenheit
+                double fahrenheit = ((1.8) * counter) + 32;
+                Console.WriteLine(fahrenheit.ToString(".0° F"));
+
+
+            }
+         
+        }
+    }
+}
---
+++ Module 1/M1HW2_Edison/KineticEnergy/M1HW2_Edison/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+/**
+* 8/23/21
+* CSC 253
+* Brandon Edison
+* This program is going to calculate kinetic energy based on user input. it will ask the user to enter mass and velocity
+* and then pass those variables to a method and return the result. 
+*/
+namespace M1HW2_Edison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call the greeting method first to greet the user
+            Greeting();
+
+            
+            //create the variables to hold the mass and the velocity from the user and ask for the input
+            Console.Write("Please enter the velocity (In meters per second) of the object: ");
+            double velocity = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Please Enter the mass (In kilograms) of the object: ");
+            double mass = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+            //create a variable to hold the return value
+            double total;
+
+            //call the method to calculate the kinetic energy
+            total = KineticEnergyCalculator(mass, velocity);
+
+            //display the total for the kientic energy and a message
+            Console.WriteLine( "\nThe amount of kinetic energy from the \nmeasurements you have entered is " + total.ToString("n") + " Joules");
+
+        }
+
+        //This method will greet the user and explain what they will need to do 
+        static void Greeting() {
+            Console.WriteLine("Greetings User! Welcome to the Kinetic Energy Calculator!");
+            Console.WriteLine();
+            Console.WriteLine("With this calculator, you will be asked to enter the mass(In kilograms) and the \n" +
+                "velocity (in meters per second) so that it will calculate the kinetic energy.");
+            Console.WriteLine();
+
+
+        }
+
+        //this method will accept the two numbers and return the kinetic energy total
+        static double KineticEnergyCalculator(double weight, double speed) {
+
+            return (.5) * weight * (speed * speed);
+        
+        }
+    }
+}
